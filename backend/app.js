e311858ecae747071ff8f25a3a2359842d3f8ae3
Abhinav <abhinav.gupta@precily.com>
2021-03-02 02:35:17 +0530

    9. Understanding CORS

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,14 @@ const express = require('express')
 
 const app = express();
 
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+;
+next();
+res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+})
+
 app.use('/api/posts',(req, res, next) => {
 console.log('first middleware!')
 
@@ -9,12 +17,12 @@ const posts = [
     {
         id:'kkjkjk',
         title:'first-server-side post',
-        contet:'this is coming from the server'
+        content:'this is coming from the server'
     },
     {
         id:'ihuh',
         title:'second-server-side post',
-        contet:'this is coming from the server!'
+        content:'this is coming from the server!'
     }
 ]
 
