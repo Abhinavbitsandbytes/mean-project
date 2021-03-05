437be47e79f5cc98d7afd4af282d0244a86a087d
Abhinav <abhinav.gupta@precily.com>
2021-03-05 22:35:24 +0530

    4. Adding Image Controls to Store the Image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -23,10 +23,11 @@ export class PostCreateComponent implements OnInit {
   ngOnInit(): void {
 
 this.form = new FormGroup({
-  'title': new FormControl(null, {
+  title: new FormControl(null, {
     validators:[Validators.required, Validators.minLength(3)]}),
-    'content': new FormControl(null, {
-      validators:[Validators.required]})
+    content: new FormControl(null, {
+      validators:[Validators.required]}),
+      image: new FormControl(null,{validators:[Validators.required]})
 })
 
 
@@ -49,6 +50,14 @@ else{
   this.postId=null;
 }
     })
+  }
+  onImagePicked(event:Event){
+    const file = (event.target as HTMLInputElement).files![0];
+   this.form.patchValue({'image':file})
+   this.form.get('image').updateValueAndValidity()
+   console.log(file);
+   console.log(this.form)
+
   }
 // its a convention to start method name with "on" that are triggered upon events
   onSavePost(){
