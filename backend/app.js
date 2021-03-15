37658906d04ec540be2dc0de3ea3728f79ed5f0c
Abhinav <abhinav.gupta@precily.com>
2021-03-15 21:41:02 +0530

    12. Fetching Images on the Frontend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const mongoose = require('mongoose')
+const path = require('path')
 
 const postRoutes = require("./routes/posts")
 const app = express();
@@ -13,6 +14,7 @@ mongoose.connect("mongodb+srv://abhinav-kumar_97:Q3M6cW3teRvYnbhi@cluster0.rayrd
     console.log('Connection failed!', error)
 })
 app.use(bodyParser.json())
+app.use("/images", express.static("backend/images"))
 
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin", "*");
