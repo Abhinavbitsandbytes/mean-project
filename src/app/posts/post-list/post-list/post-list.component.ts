4085e768f6f5a76370dee78f094f99bf98dd5f9f
Abhinav <abhinav.gupta@precily.com>
2021-03-16 16:30:44 +0530

    2. Adding the Pagination Component

--- src/app/posts/post-list/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import {Post} from '../../post.model'
 import { PostsService } from '../../posts.service';
 import {Subscription} from 'rxjs'
+import { PageEvent } from '@angular/material/paginator';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -23,6 +24,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   // ]
  posts : Post[]=[]
  isLoading=false
+ totalPosts = 10;
+ postsPerPage=2;
+ pageSizeOptions=[1,2,5,10]
  private postsSub: Subscription | any
 constructor(public postsService:PostsService){
   
@@ -44,6 +48,10 @@ this.posts=posts
   }
   onDelete(postId: string){
     this.postsService.deletePost(postId)
+  }
+
+  onChangedPage(pageData:PageEvent){
+console.log(pageData)
   }
 
 }
